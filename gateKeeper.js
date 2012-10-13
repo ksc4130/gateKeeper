6ea5422210a82b2a41c43c7ffcbdfc3c3175e740
Kyle <ksc4130@gmail.com>
2012-10-12 23:10:40 -0400

    switch

--- gateKeeper.js
+++ gateKeeper.js
@@ -1,31 +1,33 @@
 var http = require('http'),
- 	httpProxy = require('http-proxy'),
- 	sys = require('sys');
+	httpProxy = require('http-proxy'),
+	sys = require('sys');
+
+var routes = {};
+
+routes['osbrew.com'] = 'osbrew.com';
+routes['www.osbrew.com'] = 'osbrew.com';
+routes['kyngster.com'] = 'kyngster.com';
+routes['www.kyngster.com'] = 'kyngster.com';
 
 httpProxy.createServer(function (req, res, proxy) {
-  console.log(sys.inspect(req.headers.host));
-  switch(req.headers.host)
-  	{
-  		case 'osbrew.com': 
+	console.log(sys.inspect(req.headers.host));
+	var h = routes[req.headers.host];
+	switch(h)
+	{
+		case 'osbrew.com': 
 			proxy.proxyRequest(req, res, {
 				host: 'localhost',
 				port: 3000
 			});
 			break;
-		case 'www.osbrew.com': 
+		case 'kyngster.com':
 			proxy.proxyRequest(req, res, {
-				host: 'localhost',
-				port: 3000
-			});
-			break;
-	  	case 'kyngster.com':
-	  		proxy.proxyRequest(req, res, {
 				host: 'localhost',
 				port: 3001
 			});
-			break;
-  	}
-  
+		break;
+	}
+
 }).listen(80);
 
 
