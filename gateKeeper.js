348e5e781045a9c475d09badfa5cdc25ba44f53c
Kyle <ksc4130@gmail.com>
2012-10-12 20:56:28 -0400

    3000

--- gateKeeper.js
+++ gateKeeper.js
@@ -7,6 +7,6 @@ var s = httpProxy.createServer({
 		'osbrew.com': '127.0.0.1:300',
 		'www.osbrew.com': '127.0.0.1:3000',
 		'kyngster.com': '127.0.0.1:3001',
-		'www.kyngster.com'" '127.0.0.1:3001"
+		'www.kyngster.com': '127.0.0.1:3001'
 	}
 }).listen(80);
