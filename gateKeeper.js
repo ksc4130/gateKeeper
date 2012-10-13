e9c738b8ec03a48a06bce0f4f2172115652af67d
Kyle <ksc4130@gmail.com>
2012-10-12 21:21:43 -0400

    3000

--- gateKeeper.js
+++ gateKeeper.js
@@ -1,12 +1,37 @@
 var http = require('http'),
-	httpProxy = require('http-proxy');
+ 	httpProxy = require('http-proxy');
 
-var s = httpProxy.createServer({
-	hostnameOnly: true,
-	router: {
-		'osbrew.com': '127.0.0.1:300',
-		//'www.osbrew.com': '127.0.0.1:3000',
-		'kyngster.com': '127.0.0.1:3001'
-		//'www.kyngster.com': '127.0.0.1:3001'
-	}
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+httpProxy.createServer(function (req, res, proxy) {
+  //
+  // Put your custom server logic here
+  //
+  proxy.proxyRequest(req, res, {
+    host: 'localhost',
+    port: 3000
+  });
 }).listen(80);
+
+
+
+
+
+
+
+
+
+
+// var s = httpProxy.createServer({
+// 	hostnameOnly: true,
+// 	router: {
+// 		'osbrew.com': '127.0.0.1:300',
+// 		//'www.osbrew.com': '127.0.0.1:3000',
+// 		'kyngster.com': '127.0.0.1:3001'
+// 		//'www.kyngster.com': '127.0.0.1:3001'
+// 	}
+// }).listen(80);
